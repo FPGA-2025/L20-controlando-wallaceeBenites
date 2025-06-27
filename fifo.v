d221b6fe3684ff5f95821a1ceae2d475d490e3a4
Wallace Benites <148720798+wallaceeBenites@users.noreply.github.com>
2025-06-26 21:40:31 -0300

    teste

--- fifo.v
+++ fifo.v
@@ -2,20 +2,46 @@ module fifo(
     input   clk,
     input   rst_n,
 
-    // Write interface
+    
     input   wr_en,
     input   [7:0] data_in,
     output  full,
 
-    // Read interface
+    
     input   rd_en,
     output  reg [7:0] data_out,
     output  empty,
 
-    // status
-    output reg [3:0] fifo_words  // Current number of elements
+    
+    output reg [3:0] fifo_words  
 );
 
-//insira seu código aqui
+	reg [7:0] mem [0:7];         
+    reg [2:0] wr_ptr, rd_ptr;    
 
+    assign full  = (fifo_words == 8);
+    assign empty = (fifo_words == 0);
+
+    always @(posedge clk) begin
+        if (!rst_n) begin
+            wr_ptr <= 0;
+            rd_ptr <= 0;
+            fifo_words <= 0;
+        end else begin
+            if (wr_en && !full) begin
+                mem[wr_ptr] <= data_in;
+                wr_ptr <= wr_ptr + 1;
+            end
+            if (rd_en && !empty) begin
+                data_out <= mem[rd_ptr];
+                rd_ptr <= rd_ptr + 1;
+            end
+
+            case ({wr_en && !full, rd_en && !empty})
+                2'b10: fifo_words <= fifo_words + 1;
+                2'b01: fifo_words <= fifo_words - 1;
+                default: fifo_words <= fifo_words;   
+            endcase
+        end
+    end
 endmodule
--- fsm.v
+++ fsm.v
@@ -9,6 +9,33 @@ module fsm(
     input [3:0] fifo_words
 );
 
-// insira seu código aqui
+    reg [1:0] state, next_state;
 
+
+    parameter WRITING    = 2'd0;
+    parameter WAIT_STOP  = 2'd1;
+    parameter WAIT_DRAIN = 2'd2;
+
+    assign fifo_data = 8'hAA; 
+
+
+    always @(posedge clk) begin
+        state <= (!rst_n) ? WRITING : next_state;
+    end
+    always @(*) begin
+        case (state)
+            WRITING: next_state <= (fifo_words >= 4'd5) ? WAIT_STOP : WRITING;
+            WAIT_STOP: next_state <= WAIT_DRAIN;
+            WAIT_DRAIN: next_state <= (fifo_words <= 4'd2) ? WRITING : WAIT_DRAIN;
+            default: next_state = WRITING;
+        endcase
+    end
+
+    
+    always @(*) begin
+        case (state)
+            WRITING: wr_en = 1'b1;
+            default: wr_en = 1'b0;
+        endcase
+    end
 endmodule
